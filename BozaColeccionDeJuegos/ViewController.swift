0de7cb8f5198bafa0290f9509cb6cfb42c7e7463
bozanilda <nilda.boza@tecsup.edu.pe>
2024-09-30 16:04:42 -0500

    Initial Commit

---
+++ BozaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BozaColeccionDeJuegos
+//
+//  Created by Nilda Boza on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
